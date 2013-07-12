b0185f43c9d3d7a0b61f7a618f601b34fca1bc53
Jake McCrary <jake@jakemccrary.com>
2013-07-12 15:10:44 -0500

    adding license

--- project.clj
+++ project.clj
@@ -1,7 +1,7 @@
 (defproject javascript-indexer "0.1.0-SNAPSHOT"
   :description "Generates a data structure representing an index which maps function name to function implementation."
   :url "https://github.com/jakemcc/javascript-indexer/"
-  :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :license {:name "BSD 3-Clause"
+            :url "LICENSE.txt"}
   :main javascript-indexer.core
   :dependencies [[org.clojure/clojure "1.5.1"]])

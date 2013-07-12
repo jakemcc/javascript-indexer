7a3127720067033f73ed8d8be07f656d55375951
Jake McCrary <jake@jakemccrary.com>
2013-07-12 10:50:26 -0500

    Update project.clj

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject javascript-indexer "0.1.0-SNAPSHOT"
   :description "Generates a data structure representing an index which maps function name to function implementation."
-  :url ""
+  :url "https://github.com/jakemcc/javascript-indexer/"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :main javascript-indexer.core
